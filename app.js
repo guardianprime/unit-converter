ba3c8bf0ecda90f990decec66b77f889b80b636d
guardianprime <okongordian@gmail.com>
2025-09-16 21:06:35 +0100

    fix: navigation now working

--- app.js
+++ app.js
@@ -12,10 +12,46 @@ const lengthUnits = {
   mi: 1609.344,
 };
 
-function convert(value, from, to) {
+const weightUnits = {
+  mg: 0.000001,
+  g: 0.001,
+  kg: 1,
+  t: 1000,
+  oz: 0.0283495,
+  lb: 0.453592,
+};
+
+function convertLength(value, from, to) {
   let result = (value * lengthUnits[from]) / lengthUnits[to];
   console.log(result);
   return result.toFixed(2) + to;
+}
+
+function convertWeight(value, from, to) {
+  let result = (value * weightUnits[from]) / weightUnits[to];
+  return result.toFixed(2) + to;
+}
+
+function convertTemperature(value, from, to) {
+  let answer;
+
+  //converting from another unit to celsius
+  if (from === "C") {
+    answer = value;
+  } else if (from === "F") {
+    answer = (value - 32) * (5 / 9);
+  } else if (from === "K") {
+    answer = value - 273.15;
+  }
+
+  //converting from celsius to another unit
+  if (to === "C") {
+    return answer.toFixed(2);
+  } else if (to === "F") {
+    return ((answer * 9) / 5 + 32).toFixed(2);
+  } else if (to === "K") {
+    return (answer + 273.15).toFixed(2);
+  }
 }
 
 app.use(express.static("public"));
@@ -30,6 +66,10 @@ app.get("/", (req, res) => {
   res.render("homepage", { result: "", type: "length" });
 });
 
+app.get("/length", (req, res) => {
+  res.render("homepage", { result: "", type: "length" });
+});
+
 app.get("/weight", (req, res) => {
   res.render("homepage", { result: "", type: "weight" });
 });
@@ -39,10 +79,31 @@ app.get("/temperature", (req, res) => {
 });
 
 app.post("/convert", (req, res) => {
-  const { value, fromLength, toLength } = req.body;
-  console.log(value, fromLength, toLength);
-  const answer = convert(value, fromLength, toLength);
-  res.render("homepage", { result: answer, type: "length" });
+  let answer;
+  if (req.body.fromLength && req.body.toLength) {
+    answer = convertLength(
+      req.body.value,
+      req.body.fromLength,
+      req.body.toLength
+    );
+    res.render("homepage", { result: answer, type: "length" });
+  } else if (req.body.fromWeight && req.body.toWeight) {
+    answer = convertWeight(
+      req.body.value,
+      req.body.fromWeight,
+      req.body.toWeight
+    );
+    res.render("homepage", { result: answer, type: "weight" });
+  } else if (req.body.fromTemperature && req.body.toTemperature) {
+    answer = convertTemperature(
+      req.body.value,
+      req.body.fromTemperature,
+      req.body.toTemperature
+    );
+    res.render("homepage", { result: answer, type: "temperature" });
+  } else {
+    res.send("please fill all forms");
+  }
 });
 
 app.listen(PORT, () => {
